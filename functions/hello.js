13a7b1fe2b5a155bfd388f5bbc3e82a81f54a04c
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2021-03-21 20:50:02 +0530

    trying error fix

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,9 @@
 // domain/.netlify/functions/hello
 exports.handler = async = (event, context) => {
   return {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     statusCode: 200,
     body: "Hello multiverse ⭐",
   };
