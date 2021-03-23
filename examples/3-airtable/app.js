610a677d57df6f312eca5c90aeb7080947f9fd1b
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2021-03-23 22:43:32 +0530

    adding third function

--- examples/3-airtable/app.js
+++ examples/3-airtable/app.js
@@ -1,1 +1,26 @@
-const result = document.querySelector('.result')
+const result = document.querySelector(".result");
+
+const fetchProducts = async () => {
+  try {
+    const { data } = await axios.get("/api/airtable");
+    const products = data
+      .map((product) => {
+        const { id, url, name, price } = product;
+        return `<a href="product.html?id=${id}" class="product">
+        <img src="${url}" alt="${name}" />
+        <div class="info">
+          <h5>${name}</h5>
+          <h5 class="price">$${price}</h5>
+        </div>
+      </a>`;
+      })
+      .join("");
+
+    result.innerHTML = products;
+  } catch (error) {
+    console.log(error);
+    result.innerHTML = "<h2>There is an error. Please try again later.</h2>";
+  }
+};
+
+fetchProducts();
